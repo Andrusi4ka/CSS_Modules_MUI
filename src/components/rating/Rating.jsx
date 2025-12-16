cc39630cf84a1a15842c8e444bb6eea4dabf05e2
Andrusi4ka <Andrusi4ka1985@gmail.com>
2025-12-16 01:53:08 +0100

    Commit 4

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -1,6 +1,6 @@
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
-import styles from './Rating.module.scss'
+import styles from './rating.module.scss'
 
 export default function RatingSize() {
     return (
